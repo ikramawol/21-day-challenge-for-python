96e18471a0fc3e31f74a826092eec0270fc73215
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 <ikruawol.14@gmail.com>
2023-02-03 11:12:33 -0800

    practice#14|print out matrix

---
+++ print_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""print out matrix"""
+def print_matrix_integer(matrix=[[]]):
+# usi  print(j, end = " ")
+        prinng loop
+    for i in matrix:
+        for j in i:
+          t()
+# using list comprehension
+"""
+    val = "\n".join(["".join(["{:2}".format(j) for j in i]) for i in matrix])
+    print(val)
+"""
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
