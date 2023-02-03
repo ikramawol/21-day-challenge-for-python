390917fcd2c7c3a4a8cb8960d44109e44561e604
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 <ikruawol.14@gmail.com>
2023-02-03 11:37:21 -0800

    practice#17| is it leep year?

---
+++ leep_year.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# evaluate if the year entered is leep year or not
+def is_leap(year):
+    leap = False
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        return True
+
+    return leap
+
+year = int(input())
+print(is_leap(year))
